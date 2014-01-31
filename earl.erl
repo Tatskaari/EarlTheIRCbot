b0043c87fe9101cef7fe2f975a4d04d5c06b53df
Simon Cooksey <sjc80@kent.ac.uk>
2014-01-31 21:51:19 +0000

    Created `send/3` and `send/4`

--- earl.erl
+++ earl.erl
@@ -10,29 +10,39 @@ connect({ok, Socket}) ->
 connect({error, Reason}) ->
 	io:format("ERROR - Could not connect: ~s~n", [Reason]).
 connect() ->
-	connect(gen_tcp:connect("irc.cs.kent.ac.uk", 6667, [], 1000)).
+	connect(gen_tcp:connect("localhost", 6667, [], 1000)).
 	
 receive_data(Socket) ->
 	receive
-		{tcp, Socket, ":irc.cs.ukc.ac.uk NOTICE AUTH :*** No Ident response\r\n"} ->
-			ok = gen_tcp:send(Socket, "USER jfp6 jfp6 jfp6 jfp6\n\rNICK Earl\n\rJOIN #bottesting");
-		{tcp, Socket, Bin} -> 
-			bufferPid ! Bin;
-		{tcp_closed, Socket} ->
-			io:format("Connection closed.~n",[]),
-			exit(self(), normal);
+	    {tcp, Socket, ":irc.cs.ukc.ac.uk NOTICE AUTH :*** No Ident response\r\n"} ->
+		send("USER", "Sir_Earl Sir_Earl Sir_Earl Sir_Earl", Socket),
+		send("NICK", "Earl", Socket);
+	    {tcp, Socket, Bin} -> 
+		bufferPid ! Bin;
+	    {tcp_closed, Socket} ->
+		io:format("Connection closed.~n",[]),
+		exit(self(), normal);
 		{send, A} ->
-			gen_tcp:send(Socket, A)
+		gen_tcp:send(Socket, A)
 	end,
-	receive_data(Socket).
+    receive_data(Socket).
 
 buffer() ->
-	buffer([]).
+    buffer([]).
 buffer(Buffer)->
-	receive
-		Tmp1 -> Tmp1
-	end,
+    receive
+	Tmp1 -> Tmp1
+    end,
+    
+    Tmp2 = Buffer ++ [Tmp1],
+    io:format("BUGGER: ~p~n", [Tmp2]),
+    buffer(Tmp2).
 
-	Tmp2 = Buffer ++ [Tmp1],
-	io:format("BUGGER: ~p~n", [Tmp2]),
-	buffer(Tmp2).
+
+% For directed messages
+send(Command, Target, Message, Socket) ->
+    ok = gen_tcp:send(Socket, Command ++ " " ++ Target ++ " " ++ Message ++ "~n").
+
+% For non-directed server comands, eg NICK and USER
+send(Command, Message, Socket) ->
+    ok = gen_tcp:send(Socket, Command ++ " " ++ Message ++ "~n").
