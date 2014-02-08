d8d94981b48cdd6f976d86a51c4434d7824b27c6
Tatskaari <jony1710@hotmail.co.uk>
2014-02-08 05:23:25 +0000

    Added hour, minute and second format option you can now call reminder like: or or the old still works

--- reminder.erl
+++ reminder.erl
@@ -8,9 +8,8 @@ reminder() ->
 	receive
 		#privmsg{target=Target, from=From, message="#reminder " ++ Rest} ->
 			{Time, Reminder} = parse(Rest),
-			io:format("~p~n",[Time]),
 			if
-				Time < 0 ->
+				Time == error ->
 					Message = From ++ ": Input error.",
 					sendPid ! #privmsg{from=From, target=Target, message=Message},
 					reminder();
@@ -25,17 +24,53 @@ reminder() ->
 % tokenises the params
 parse(Rest) ->
 	P = string:str(Rest, " "),
-	{Time, Reminder} = {string:substr(Rest, 1, P-1), string:substr(Rest, P+1)},
-	{stringToInt(Time), Reminder}.
+	{TimeString, Reminder} = {string:substr(Rest, 1, P-1), string:substr(Rest, P+1)},
+	Time = parse(TimeString, time),
+	{Time, Reminder}.
+parse(Time, time) ->
+	case string:tokens(Time, ":") of
+		[Hours, Mins, Secs] ->
+			Hour = stringToInt(Hours),
+			Min = stringToInt(Mins),
+			Sec = stringToInt(Secs),
+			timeToSec({Hour, Min, Sec});
+		[Mins, Secs] ->
+			Hour = 0,
+			Min = stringToInt(Mins),
+			Sec = stringToInt(Secs),
+			timeToSec({Hour, Min, Sec});
+		[Secs] ->
+			Hour = 0,
+			Min = 0,
+			Sec = stringToInt(Secs),
+			timeToSec({Hour, Min, Sec});
+		_ ->
+			0
+	end.
+timeToSec({Hour, Min, Sec}) ->
+	if 
+		(Hour =/= error) and (Min =/= error) and (Sec =/= error) ->
+			Hour * 60 * 60 + Min * 60 + Sec;
+		true ->
+			error
+	end.
+
 
 % converts a list of ints into the integer they represent 
 stringToInt(Str) ->
 	case string:to_integer(Str) of
-        {error, _} -> -1;
-        {F,_Rest} -> F
+        {error, _} -> 
+        	error;
+        {F,_Rest} -> 
+        	if
+        		F < 0 ->
+        			error;
+        		true ->
+        			F
+        	end
     end.
 
 echoIn(Time, Message, {To, Target})->
 	timer:sleep(Time * 1000),
-	sendPid ! #privmsg{from=To, target=Target, message=Message}.
+	sendPid ! #privmsg{from=To, target=Target, message=To ++ ": " ++ Message}.
 	
