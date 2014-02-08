7e04593d7fc79903d288cd7e248e4179543ef19b
Simon Cooksey <sjc80@kent.ac.uk>
2014-02-08 00:45:20 +0000

    Added new files to the compile script and broke the line to make it a little less unweildy.

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-erlc earl.erl messageRouter.erl ircParser.erl optimusPrime.erl ircTime.erl telnet.erl earlConnection.erl earlAdminPlugin.erl && \
+erlc earl.erl settingsServer.erl messageRouter.erl ircParser.erl \
+optimusPrime.erl ircTime.erl telnet.erl earlConnection.erl earlAdminPlugin.erl && \
 erl -s earl main -s init stop
