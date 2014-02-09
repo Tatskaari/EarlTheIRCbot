2babb217e3b87c7303db11109ae4db595aca35f3
Tatskaari <jony1710@hotmail.co.uk>
2014-02-09 18:01:06 +0000

    Added reminding at date. Refactored code so the egg timer style reminder doesn't worka anymore.

--- reminder.erl
+++ reminder.erl
@@ -1,5 +1,5 @@
 -module(reminder).
--export([reminder/0, parse/1]).
+-export([reminder/0, dateParser/1]).
 
 %Contains the record definitions
 -include("ircParser.hrl").
@@ -7,46 +7,72 @@
 reminder() ->
 	receive
 		#privmsg{target=Target, from=From, message="#reminder " ++ Rest} ->
-			{Time, Reminder} = parse(Rest),
-			if
-				Time == error ->
-					Message = From ++ ": Input error.",
-					sendPid ! #privmsg{from=From, target=Target, message=Message},
-					reminder();
-				true ->
-					spawn(fun() -> echoIn(Time, Reminder, {From, Target}) end)
+			case dateParser(Rest) of
+				{GregorianTime, Reminder} ->
+					echoAt(GregorianTime, Reminder, {From, Target});
+				_ ->
+					#privmsg{target=Target, from=From, message=From ++ ": Input error"}
 			end;
 		die ->
 			io:format("reminder :: EXIT")
 	end,
 	reminder().
 
-% tokenises the params
-parse(Rest) ->
-	P = string:str(Rest, " "),
-	{TimeString, Reminder} = {string:substr(Rest, 1, P-1), string:substr(Rest, P+1)},
-	Time = parse(TimeString, time),
-	{Time, Reminder}.
-parse(Time, time) ->
-	case string:tokens(Time, ":") of
-		[Hours, Mins, Secs] ->
-			Hour = stringToInt(Hours),
-			Min = stringToInt(Mins),
-			Sec = stringToInt(Secs),
-			timeToSec({Hour, Min, Sec});
-		[Mins, Secs] ->
-			Hour = 0,
-			Min = stringToInt(Mins),
-			Sec = stringToInt(Secs),
-			timeToSec({Hour, Min, Sec});
-		[Secs] ->
-			Hour = 0,
-			Min = 0,
-			Sec = stringToInt(Secs),
-			timeToSec({Hour, Min, Sec});
-		_ ->
-			0
+echoIn(Time, Message, {To, Target}) ->
+	timer:sleep(Time * 1000),
+	sendPid ! #privmsg{from=To, target=Target, message=To ++ ": " ++ Message}.
+echoAt(Time, Message, {To, Target}) ->
+	CurrentTime = calendar:datetime_to_gregorian_seconds(erlang:localtime()),
+	SleepTime = Time - CurrentTime,
+	if
+		SleepTime < 0 ->
+			sendPid ! #privmsg{from=To, target=Target, message=To ++ ": Error, time is in the past"};
+		true ->
+			timer:sleep(SleepTime * 1000),
+			sendPid ! #privmsg{from=To, target=Target, message=To ++ ": Reminder, '" ++ Message ++ "'"}
 	end.
+
+% converts an inputted string into a date time stamp and the reminder
+dateParser(String) ->
+	{Time, Rest} = token(String, " "),
+	{Date, Reminder} = token(Rest, " "),
+	DateTuple = dateStringToTuple(Date),
+	TimeTuple = timeStringToTuple(Time),
+	case (validDate(DateTuple) and validTime(TimeTuple)) of
+		true ->
+			{calendar:datetime_to_gregorian_seconds({DateTuple, TimeTuple}), Reminder};
+		_ ->
+			error
+	end.
+		
+
+timeStringToTuple(TimeString) ->
+	case string:tokens(TimeString, ":") of
+		[Hour, Min, Sec] ->
+			{stringToInt(Hour), stringToInt(Min), stringToInt(Sec)};
+		[Min, Sec] ->
+			{0, stringToInt(Min), stringToInt(Sec)};
+		[Sec] ->
+			{0, 0, stringToInt(Sec)};
+		_ -> 
+			error
+	end.
+
+dateStringToTuple(DateSting) ->
+	case string:tokens(DateSting, "/") of
+		[Day, Month, Year] ->
+			{stringToInt(Year), stringToInt(Month), stringToInt(Day)};
+		_ -> 
+			error
+	end.
+
+% utils
+
+% gets the first token in String splitting on Splitter.
+token(String, Splitter) ->
+	P = string:str(String, Splitter),
+	{Token, Rest} = {string:substr(String, 1, P-1), string:substr(String, P+1)}.
+
 timeToSec({Hour, Min, Sec}) ->
 	if 
 		(Hour =/= error) and (Min =/= error) and (Sec =/= error) ->
@@ -54,7 +80,6 @@ timeToSec({Hour, Min, Sec}) ->
 		true ->
 			error
 	end.
-
 
 % converts a list of ints into the integer they represent 
 stringToInt(Str) ->
@@ -70,7 +95,17 @@ stringToInt(Str) ->
         	end
     end.
 
-echoIn(Time, Message, {To, Target})->
-	timer:sleep(Time * 1000),
-	sendPid ! #privmsg{from=To, target=Target, message=To ++ ": " ++ Message}.
-	
+validTime({Hour, Min, Sec}) ->
+	if
+		(Hour > 24) or (Min > 59) or (Sec > 59)->
+			false;
+		true ->
+			true
+	end;
+validTime(_) ->
+	false.
+
+validDate({Year, Month, Day}) ->
+	calendar:valid_date({Year, Month, Day});
+validDate(_) ->
+	false.
